6c4bd49db1947b205068026f7b7342fb77ea0223
seoe5580 <seoe5580@wrdsb.ca>
2020-10-13 04:13:38 +0000

    Actually done ascending order question

--- Main.java
+++ Main.java
@@ -16,14 +16,19 @@ public class Main {
      // Asks the user to input 2 integers
     System.out.println("Please enter the 2 values for the array");
 
+    // sets up array
     int[] num = new int[2];
 
+    // gets the two numbers from the user using the for loop
     for(int i = 0; i < num.length; i++){
       num[i] = input.nextInt();
     }
 
+    // sets up a variable for future product
     int num1 = num[0];
     int num2 = num[1];
+
+    // using if else statements tells the user the answer
     if(num[0] < num[1]){
       System.out.println("Your numbers in ascending order are " + num1 + " and " + num2);
     }else{
